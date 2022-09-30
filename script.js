262520ebbae801023ad843b080661ac149dd6a73
ceceukoha <ceceukoha@gmail.com>
2022-09-30 03:56:48 -0400

    New files

---
+++ script.js
@@ -0,0 +1,86 @@
+const menuOne = document.querySelector('#menu-bar');
+const exitOne = document.querySelector('#close-menu');
+const mainMenu = document.querySelector('.menu');
+
+
+function openMenu(){
+mainMenu.style.width = "100px";
+mainMenu.style.display = "block";
+mainMenu.style.width = "100px";
+}
+function openMenu(){
+    mainMenu.style.width = "100px";
+    mainMenu.style.display = "block";
+    mainMenu.style.width = "100px";
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function clickHandler(event){
+//     mainMenu.classList.add('animate__animated', 'animate__slideInLeft');
+// }
+// menuOne.addEventListener('click', clickHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function clickHandler(event){
+
+// }
+// menuOne.addEventListener('click', clickHandler )
+
+
