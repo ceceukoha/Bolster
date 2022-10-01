24768f5f1d64164dae9947bc923c7a8f3ffd6aed
ceceukoha <ceceukoha@gmail.com>
2022-10-01 15:39:48 -0400

    new

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const menuOne = document.querySelector('#menu-bar');
 const exitOne = document.querySelector('#close-menu');
 const mainMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mob-menu');
-const leafy = document.querySelector('#logo');
-
 
 
 menuOne.addEventListener("click",(e)=>{
@@ -15,22 +12,6 @@ exitOne.addEventListener("click",(e)=>{
     // mainMenu.style.display = 'none';
     // mainMenu.classList.remove('animate__animated', 'animate__slideIntLeft')
     mainMenu.classList.add('animate__animated', 'animate__slideOutLeft')
-    
-})
-leafy.addEventListener("click",(e)=>{
-    // mainMenu.style.display = 'none';
-    // mainMenu.classList.remove('animate__animated', 'animate__slideIntLeft')
-    mainMenu.style.display ="block"
-    mainMenu.classList.add('animate__animated', 'animate__slideInDown')
-    
-})
-
-
-mobileMenu.addEventListener("click",(e)=>{
-    // mainMenu.style.display = 'none';
-    // mainMenu.classList.remove('animate__animated', 'animate__slideIntLeft')
-    mainMenu.style.display ="block"
-    mainMenu.classList.add('animate__animated', 'animate__slideInDown')
     
 })
 
