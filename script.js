ea832b77bed3c262c5577407c000fa1cab06bd1d
ceceukoha <ceceukoha@gmail.com>
2022-10-01 18:00:17 -0400

    linked  login and register pages

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const menuOne = document.querySelector('#menu-bar');
 const exitOne = document.querySelector('#close-menu');
 const mainMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mob-menu');
+const logos = document.querySelector('#logo');
 
 
 menuOne.addEventListener("click",(e)=>{
-    // mainMenu.style.display = 'block';
+    mainMenu.style.display = 'block';
     // mainMenu.classList.remove('animate__animated', 'animate__slideOutLeft')
     mainMenu.classList.add('animate__animated', 'animate__slideInLeft')
 })
@@ -14,7 +16,38 @@ exitOne.addEventListener("click",(e)=>{
     mainMenu.classList.add('animate__animated', 'animate__slideOutLeft')
     
 })
+logos.addEventListener("click",(e)=>{
+    mainMenu.style.display = 'block';
+    // mainMenu.classList.remove('animate__animated', 'animate__slideIntLeft')
+    mainMenu.classList.add('animate__animated', 'animate__slideOutLeft')
+    
+})
+mobileMenu.addEventListener("click",(e)=>{
+    mainMenu.style.display = 'block';
+    // mainMenu.classList.remove('animate__animated', 'animate__slideIntLeft')
+    mainMenu.classList.add('animate__animated', 'animate__slideInLeft')
+    
+})
 
+
+
+// const callback = (entries) => {
+//     entries.forEach(
+//         (entry) => {
+//             if (entry.isIntersecting) {
+//                 console.log('This is intersecting')
+//             }
+//         }
+//     )
+// }
+
+// let observer = new IntersectionObserver(callback);
+
+const animationItems = document.querySelectorAll('.animate__animated');
+
+animationItems.forEach(item => {
+    observer.observe(item)
+})
 
 
 
